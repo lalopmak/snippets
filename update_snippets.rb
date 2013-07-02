801ab3bf36417f814417b7476c7fa8aa545e47bf
lalopmak <lalop.llmao@gmail.com>
2013-07-01 21:27:38 -0500

    Made update_snippets.rb independent of running directory

--- update_snippets.rb
+++ update_snippets.rb
@@ -34,25 +34,27 @@ end
 #misc files to copy over if found
 copy_files = [".yas-parents",  ".yas-ignore-filenames-as-triggers", ".yas-make-groups"]
 
-#copies each subdirectory into that yasnippet directory
-Dir.foreach("."){ |directory|
-  if directory != "." and directory != ".." and File.directory?(directory)
-
-    dest_dir = File.join(yasnippet_dir,directory)
-    if not File.exists?(dest_dir)
-      FileUtils.mkdir_p(dest_dir)
+#our working directory is this script's directory
+Dir.chdir(File.dirname(File.expand_path(__FILE__))) do
+  #copies each subdirectory into that yasnippet directory
+  Dir.foreach("."){ |directory|
+    if File.directory?(directory) and directory != "." and directory != ".." and directory != ".git" 
+      #gets (and makes, if necessary) its destination directory
+      dest_dir = File.join(yasnippet_dir,directory)
+      if not File.exists?(dest_dir)
+        FileUtils.mkdir_p(dest_dir)
+      end
+     
+      Dir.chdir(directory) do 
+        Dir.foreach("."){ |file|
+          #only copies .yasnippet files or certain copy_files
+          if File.extname(file) == ".yasnippet" or copy_files.include?(file)
+            FileUtils.copy(file,File.join(dest_dir,file))
+          end
+        }
+      end
+      
+      puts "Copied from " + File.absolute_path(directory) + " to " + File.absolute_path(dest_dir)
     end
-    
-    Dir.chdir(directory) do 
-      Dir.foreach("."){ |file|
-        #only copies .yasnippet files or certain copy_files
-        if File.extname(file) == ".yasnippet" or copy_files.include?(file)
-          FileUtils.copy(file,File.join(dest_dir,file))
-        end
-      }
-    end
-    
-    puts "Copied from " + directory + " to " + dest_dir
-  end
-}
-
+  }
+end
